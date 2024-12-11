f2e742b20523b59d97ca84353054a7ae2047a14d
s3warriors <satveerswami982@gmail.com>
2024-12-11 09:58:43 +0530

    Enhance BST to support dynamic insertion of multiple nodes

---
+++ src/BinaryTreeWithNodes.java
@@ -0,0 +1,20 @@
+public class BinaryTreeWithNodes {
+    public static void main(String[] args) {
+        MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+
+        System.out.println("Size of the binary tree: " + binaryTree.getSize());
+    }
+}
